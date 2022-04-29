4d993648634ae3f7771a01a24bc698bb64f08c5e
Alexander <shymbabigmir@gmail.com>
2022-04-29 19:10:11 +0300

    test

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  // publicPath: '/star-DB-vue/',
+  publicPath: '/star-DB-vue/',
   transpileDependencies: true
 })
