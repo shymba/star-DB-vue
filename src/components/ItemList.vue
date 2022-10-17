777533fc2fdcbebc377f758a4b2d3b9ddc847861
Alexander <shymbabigmir@gmail.com>
2022-10-17 17:47:42 +0300

    Added pagination in the list of people items

--- src/components/ItemList.vue
+++ src/components/ItemList.vue
@@ -1,28 +1,51 @@
 <template>
   <div class="list">
-    <ul v-if="swObjects[0]">
-      <li
-          @click="$emit('select', person)"
-          v-for="person in this.swObjects"
-          :key="person">
-        {{ person.name }}
+<!--    <ul v-if="swObjects[0]">-->
+<!--      <li-->
+<!--          @click="$emit('select', person)"-->
+<!--          v-for="person in this.swObjects"-->
+<!--          :key="person">-->
+<!--        {{ person.name }}-->
+<!--      </li>-->
+<!--    </ul>-->
+    <div class="spinner-list" v-if="!loading">
+      <Spinner />
+    </div>
+
+    <ul v-else>
+      <li v-for="name in people" :key="name">
+        {{name.name}}
       </li>
     </ul>
-    <div class="spinner-list"
-        v-else
-    >
-      <Spinner />
+    <div class="page-wrapper">
+      <div
+          @click="changePage(pageNumber)"
+          class="pages"
+          v-for="pageNumber in totalPages"
+          :key="pageNumber">
+        {{ pageNumber }}
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import Spinner from "@/components/Spinner";
+import axios from 'axios';
 
 export default {
   name: "ItemList",
   components: {
     Spinner
+  },
+  data() {
+    return {
+      people: [],
+      totalPages: 0,
+      limit: 10,
+      page: 1,
+      loading: true
+    }
   },
   props: {
     swObjects: {
@@ -31,8 +54,25 @@ export default {
     },
   },
   methods: {
+    changePage(pageNum) {
+      this.page = pageNum;
+      this.getPeople();
+    },
+
+    async getPeople() {
+      this.loading = false;
+      const response = await axios.get('https://swapi.dev/api/people/', {
+        params: {
+          page: this.page
+        }
+      });
+      this.totalPages = Math.ceil(response.data.count / this.limit);
+      this.people = response.data.results;
+      this.loading = true
+    },
   },
   mounted() {
+    this.getPeople();
   }
 }
 </script>
@@ -75,6 +115,21 @@ export default {
   padding: .75rem 1.25rem;
   cursor: pointer;
   background-color: #444;
+}
+.page-wrapper {
+  display: flex;
+  justify-content: center;
+  margin: 15px 0;
+}
+
+.pages {
+  color: white;
+  cursor: pointer;
+  font-size: 14px;
+  padding: 6px;
+  border-radius: 5px;
+  border: 2px solid black;
+  margin: 2px;
 }
 
 </style>
